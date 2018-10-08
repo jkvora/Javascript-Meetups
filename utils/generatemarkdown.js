fb57d62bab697c04bf16fc04b04a330d5eb1063d
jkvora <vorajalak@yahoo.co.in>
2018-10-08 23:15:48 +0530

    Error handling in making directories

--- utils/generatemarkdown.js
+++ utils/generatemarkdown.js
@@ -21,30 +21,31 @@ generateMarkdown = () => {
 
 generateFolders = lstMeetups => {
   for (let city in lstMeetups) {
-    makeDirectory('meetups/' + city);
-
-    for (let event in lstMeetups[city]) {
-      makeDirectory('meetups/' + city + '/' + event);
-      let mdchunk = '';
-      for (let k = 0; k < lstMeetups[city][event].length; k++) {
-        mdchunk =
-          mdchunk +
-          json2md([
-            { h1: lstMeetups[city][event][k].talk },
-            { blockquote: lstMeetups[city][event][k].name },
-            {
-              ul: [
-                'Twitter:' + lstMeetups[city][event][k].twitter,
-                'Resource :' + lstMeetups[city][event][k].resource
-              ]
-            }
-          ]);
+    if (makeDirectory('meetups/' + city)) {
+      for (let event in lstMeetups[city]) {
+        if (makeDirectory('meetups/' + city + '/' + event)) {
+          let mdchunk = '';
+          for (let k = 0; k < lstMeetups[city][event].length; k++) {
+            mdchunk =
+              mdchunk +
+              json2md([
+                { h1: lstMeetups[city][event][k].talk },
+                { blockquote: lstMeetups[city][event][k].name },
+                {
+                  ul: [
+                    'Twitter:' + lstMeetups[city][event][k].twitter,
+                    'Resource :' + lstMeetups[city][event][k].resource
+                  ]
+                }
+              ]);
+          }
+          fs.writeFileSync(
+            'meetups/' + city + '/' + event + '/' + 'talks.md',
+            mdchunk,
+            'utf-8'
+          );
+        }
       }
-      fs.writeFileSync(
-        'meetups/' + city + '/' + event + '/' + 'talks.md',
-        mdchunk,
-        'utf-8'
-      );
     }
   }
 };
@@ -53,12 +54,11 @@ makeDirectory = path => {
   if (!fs.existsSync(path)) {
     fs.mkdirSync(path, 0766, function(err) {
       if (err) {
-        console.log(err);
-        response.send("ERROR! Can't make the directory! \n");
+        return false;
       }
     });
   }
+  return true;
 };
 
-generateMarkdown();
 module.exports = generateMarkdown;
